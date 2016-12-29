2cc7a13d537924b94d46d49b125777ca7f95c3a6
die2live <jcswaiting4u@gmail.com>
2016-12-29 10:05:47 +0200

    updated google analytics ID

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = False
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = "UA-58575466-1"
+GOOGLE_ANALYTICS = "UA-89570465-1"
